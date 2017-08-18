32d69cba06623896af7ef3c1a516514b568a868d
Luiz Parreira <luizfilipester@gmail.com>
2017-08-17 22:35:59 -0300

    Changed transaction params parsing to `params_caster`

--- lib/bank_api/bank/credit.ex
+++ lib/bank_api/bank/credit.ex
@@ -4,41 +4,11 @@ defmodule BankApi.Bank.Credit do
   """
 
   alias BankApi.Bank
+  alias BankApi.Bank.ParamsCaster
 
   def credit(params) do
     params
-    |> process_params
+    |> ParamsCaster.cast
     |> Bank.create_transaction
   end
-
-  defp process_params(%{amount: amount, date: date, description: desc, checking_account_id: id}) do
-    %{amount: validate_amount(amount),
-      date: parse_date(date),
-      description: desc,
-      checking_account_id: id }
-  end
-  defp process_params(_), do: %{}
-
-  def validate_amount(nil), do: nil
-  def validate_amount(amount) do
-    if String.to_float(amount) >= 0, do: amount, else: nil
-  end
-  def validate_amount(_), do: nil
-
-
-  def parse_date(nil), do: nil
-  def parse_date(date) when is_integer(date) do
-    date
-    |> DateTime.from_unix
-    |> _parse_date
-  end
-
-  def parse_date(date) when is_bitstring(date) do
-    date
-    |> String.to_integer
-    |> DateTime.from_unix
-  end
-
-  defp _parse_date({:ok, date}), do: date
-  defp _parse_date({:error, _error}), do: nil
 end
--- test/bank_api/bank/credit_test.exs
+++ test/bank_api/bank/credit_test.exs
@@ -1,12 +1,10 @@
-
 defmodule BankApi.CreditTest do
   use BankApi.DataCase
 
-  alias BankApi.Bank.Credit
   alias BankApi.Bank
+  alias BankApi.Bank.Transaction
 
   describe "credit/1" do
-    alias BankApi.Bank.Transaction
 
     @valid_attrs %{
       amount: "120.5",
@@ -18,22 +16,27 @@ defmodule BankApi.CreditTest do
 
     setup do
       {:ok, account} = Bank.create_checking_account(%{name: "some name"})
-      {:ok, %{params: %{ @valid_attrs | checking_account_id: account.id }}}
+      {:ok, %{params: %{@valid_attrs | checking_account_id: account.id}}}
     end
 
     test "with valid data creates credit transaction", %{params: params} do
-      assert {:ok, %Transaction{} = transaction} = Credit.credit(params)
+      assert {:ok, %Transaction{} = transaction} = Bank.credit(params)
       assert transaction.amount == Decimal.new("120.5")
       assert transaction.date == DateTime.from_naive!(~N[2010-04-17 14:00:00Z], "Etc/UTC")
       assert transaction.description == "some description"
     end
 
+    test "when date is a string timestamp", %{params: params} do
+      assert {:ok, %Transaction{} = transaction} = Bank.credit(%{params | date: ~N[2010-04-17 14:00:00Z]})
+      assert transaction.date == DateTime.from_naive!(~N[2010-04-17 14:00:00Z], "Etc/UTC")
+    end
+
     test "trying to credit a negative amount of the user's account", %{params: params} do
-      assert {:error, %Ecto.Changeset{}} = Credit.credit(%{params | amount: "-100.0" })
+      assert {:error, %Ecto.Changeset{}} = Bank.credit(%{params | amount: "-100.0"})
     end
 
     test "credit/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Credit.credit(@invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Bank.credit(@invalid_attrs)
     end
   end
 end
