483fad8f2a244295ad08fcb602c191783c97c402
Luiz Parreira <luizfilipester@gmail.com>
2017-08-19 14:23:53 -0300

    Removed unused files and also finished implementing transactions endpoint

--- lib/bank_api/web/controllers/transactions_controller.ex
+++ lib/bank_api/web/controllers/transactions_controller.ex
@@ -2,14 +2,27 @@ defmodule BankApi.Web.TransactionsController do
   use BankApi.Web, :controller
 
   alias BankApi.Bank
-  alias BankApi.Bank.CheckingAccount
-  alias BankApi.Bank.Transaction
+  alias Bank.CheckingAccount
+  alias Bank.Transaction
+  alias Bank.AmountValidator
 
-  def credit(conn, %{"checking_account_id" => id, "date" => date, "description" => desc, "amount" => amount}) do
+  action_fallback BankApi.Web.FallbackController
+
+  def credit(conn, params) do
+    transact(conn, :credit, params)
+  end
+
+  def debit(conn, params) do
+    transact(conn, :debit, params)
+  end
+
+  defp transact(conn, op, %{"checking_account_id" => id, "date" => date, "description" => desc, "amount" => amount}) do
     with %CheckingAccount{} <- Bank.get_checking_account(id),
+         {date, _} <- Integer.parse(date),
          {:ok, date} <- DateTime.from_unix(date),
-         %Transaction{} <- Bank.credit(id, amount, date, desc) do
-      render(conn, %{data: "success"})
+         {:ok, amount} <- AmountValidator.validate(amount, op),
+         (transaction = %Transaction{}) <- Bank.transact(id, amount, date, desc) do
+      render(conn, "success.json", transaction: transaction)
     else
       nil -> {:error, :not_found}
        _  -> {:error, :bad_request}
