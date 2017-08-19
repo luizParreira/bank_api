19ddfeab539da749edfb06ecf3eb01ebe7de5701
Luiz Parreira <luizfilipester@gmail.com>
2017-08-19 14:27:41 -0300

    Implemented module that validates whether a given amount is negative and returns the proper amount for `credit` and `debit` operations, since `credit` will have a positive amount saved to the db, whereas `debit` should have a negative amount

---
+++ lib/bank_api/bank/amount_validator.ex
@@ -0,0 +1,16 @@
+defmodule BankApi.Bank.AmountValidator do
+  @moduledoc """
+  Helper module responsible for validating if the amount passed in as param in the API is positive
+  """
+
+  def validate(amount, op) do
+    amount
+    |> Float.parse
+    |> _validate(op)
+  end
+
+  def _validate(:error, _op), do: :error
+  def _validate({amount, _}, operations) when amount <= 0, do: :error
+  def _validate({amount, _}, :credit), do: {:ok, amount}
+  def _validate({amount, _}, :debit), do: {:ok, -amount}
+end
