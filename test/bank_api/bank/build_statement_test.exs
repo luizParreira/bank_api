90cea1816e60dabf8d8e63acee8518ab33949ebb
Luiz Parreira <luizfilipester@gmail.com>
2017-08-20 19:09:32 -0300

    Implemented `transactions_helper` to aid in the creation of transactions for testing purposes

--- test/bank_api/bank/build_statement_test.exs
+++ test/bank_api/bank/build_statement_test.exs
@@ -3,51 +3,11 @@ defmodule BankApi.BuildStatementTest do
 
   alias BankApi.Bank
   alias Bank.BuildStatement
-  @account %{name: "Jhon Snow"}
-  @transaction_1 %{
-    description: "Deposit",
-    date: DateTime.from_naive!(~N[2010-04-15 14:00:00.000000Z], "Etc/UTC"),
-    amount: "150.0",
-    checking_account_id: nil}
-
-  @transaction_2 %{
-    description: "Went out for drinks",
-    date: DateTime.from_naive!(~N[2010-04-17 14:00:00.000000Z], "Etc/UTC"),
-    amount: "-150.0",
-    checking_account_id: nil}
-
-  @transaction_3 %{
-    description: "Deposit",
-    date: DateTime.from_naive!(~N[2010-04-19 20:00:00.000000Z], "Etc/UTC"),
-    amount: "200.5",
-    checking_account_id: nil}
-
-  @transaction_4 %{
-    description: "Bought books on Amazon",
-    date: DateTime.from_naive!(~N[2010-04-18 14:00:00.000000Z], "Etc/UTC"),
-    amount: "-150.0",
-    checking_account_id: nil}
-
-  @transaction_5 %{
-    description: "payment from Jose",
-    date: DateTime.from_naive!(~N[2010-03-17 14:00:00.000000Z], "Etc/UTC"),
-    amount: "302.0",
-    checking_account_id: nil}
-
-  @transaction_6 %{
-    description: "payment from Ana",
-    date: DateTime.from_naive!(~N[2010-04-17 23:59:00.000000Z], "Etc/UTC"),
-    amount: "213.5",
-    checking_account_id: nil}
-
-
-  @transactions [@transaction_1, @transaction_2, @transaction_3, @transaction_4, @transaction_5, @transaction_6]
+  alias BankApi.TransactionsHelper
 
   setup do
-    {:ok, account} = Bank.create_checking_account(@account)
-    @transactions
-    |> Enum.map(&(Bank.create_transaction(%{&1 | checking_account_id: account.id})))
-
+    {:ok, account} = Bank.create_checking_account(%{name: "Jhon snow"})
+    TransactionsHelper.create_transactions(account.id)
     {:ok, id: account.id}
   end
 
