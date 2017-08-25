51ced0e096c239a93515753898c54e4e6a34ced7
Luiz Parreira <luizfilipester@gmail.com>
2017-08-24 22:33:42 -0300

    [wip] Creating script to populate the db

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,36 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+#
+#
+#
+alias BankApi.Bank
+alias BankApi.Repo
+
+BankApi.Repo.transaction(fn ->
+  {:ok, account} = Bank.create_checking_account(%{name: "Account"})
+  {:ok, account1} = Bank.create_checking_account(%{name: "Account 1"})
+  _ = Bank.create_checking_account(%{name: "Account 2"})
+  _ = Bank.create_checking_account(%{name: "Account 3"})
+
+  descriptions = ["Deposit", "Buy books", "Deposit", "Buy food", "buy drinks", "Payment from Joe"]
+  amounts = [324.3, -223.5, 300.0, -234.5, -77.34, 400.0]
+  dates = [
+    DateTime.from_naive!(~N[2016-03-24 13:26:08.003], "Etc/UTC"),
+    DateTime.from_naive!(~N[2016-04-29 13:26:08.003], "Etc/UTC"),
+    DateTime.from_naive!(~N[2016-06-24 13:26:08.003], "Etc/UTC"),
+    DateTime.from_naive!(~N[2016-11-24 13:26:08.003], "Etc/UTC"),
+    DateTime.from_naive!(~N[2017-06-10 13:26:08.003], "Etc/UTC"),
+    DateTime.from_naive!(~N[2017-08-12 13:26:08.003], "Etc/UTC")
+  ]
+  0..5
+  |> Enum.map(fn index ->
+    Bank.create_transaction(
+      %{checking_account_id: account.id,
+      date: dates[index],
+      amount: amounts[index],
+      description: description})
+  end)
+
+  BankApi.TransactionHelper.create_transactions(account1.id)
+end)
